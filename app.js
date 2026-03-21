41f4a83f0ae4d31c4cea03070898676cf4937ce6
ARI VPN STORES <arivpnstores@gmail.com>
2026-03-22 01:04:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3795,6 +3795,7 @@ app.listen(port, () => {
       logger.info('Bot telah dimulai');
   }).catch((error) => {
       logger.error('Error saat memulai bot:', error);
+      process.exit(1);
   });
   logger.info(`Server berjalan di port ${port}`);
 });
